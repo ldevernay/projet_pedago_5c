ce681c9706d221357cb587c416e497745e865341
RashidMall <mr.mallaev@gmail.com>
2018-07-10 14:18:22 +0200

    Add routes

--- app/app.js
+++ app/app.js
@@ -1,3 +1,30 @@
 const express = require('express');
 const app = express();
-const 
+
+app.use(express.static('public'));
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index');
+    console.log('Load index.ejs');
+});
+
+app.get('/planter', (req, res) => {
+    res.render('planter');
+    console.log('Load planter.ejs');
+});
+
+app.get('/manger', (req, res) => {
+    res.render('manger');
+    console.log('Load manger.ejs');
+});
+
+app.get('/bouger', (req, res) => {
+    res.render('bouger');
+    console.log('Load bouger.ejs');
+});
+
+const server = app.listen(process.env.PORT || 8080, (req, res) => {
+    console.log('Server Ok!');
+});
