b009af2d76373b7f4a2915e7d7219a6ca12d839a
RashidMall <mr.mallaev@gmail.com>
2018-07-10 16:14:11 +0200

    Add again

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync');
+var nodemon = require('gulp-nodemon');
 var useref = require('gulp-useref');
 var uglify = require('gulp-uglify');
 
@@ -24,16 +25,24 @@ gulp.task('useref', function(){
         .pipe(gulp.dest('dist'))
 });
 
-gulp.task('browserSync', function(){
-    browserSync({
-        server: {
-            baseDir: 'app'
-        },
-    })
+gulp.task('browserSync', ['nodemon'], function() {
+    browserSync.init(null, {
+        proxy: "http://localhost:8080",
+    });
+});
+
+gulp.task('nodemon', function (cb) {
+    var callbackCalled = false;
+    return nodemon({script: './app/app.js'}).on('start', function () {
+        if (!callbackCalled) {
+            callbackCalled = true;
+            cb();
+        }
+    });
 });
 
 gulp.task('watch', ['browserSync', 'sass'], function(){
     gulp.watch('app/scss/**/*.scss', ['sass']);
-    // gulp.watch('app/*.html', browserSync.reload);
-    // gulp.watch('app/js/**/*.js', browserSync.reload);
+    gulp.watch('app/app.js', browserSync.reload);
+    gulp.watch('app/views/*.ejs', browserSync.reload);
 })
