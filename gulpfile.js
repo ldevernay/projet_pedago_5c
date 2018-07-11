d7374a09d1ef401445252368858954de48c0b856
RashidMall <mr.mallaev@gmail.com>
2018-07-11 10:02:01 +0200

    Modify folders configuration to fix gulp bug

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,64 @@
 var gulp = require('gulp');
+var compass = require('gulp-compass');
+// Requires the gulp-sass plugin
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync');
+var reload = browserSync.reload;
 var nodemon = require('gulp-nodemon');
-var useref = require('gulp-useref');
-var uglify = require('gulp-uglify');
 
-gulp.task('sass', function(){
-    return gulp.src('app/scss/**/*.scss')
+var app = './'; // dossier de travail
+var destination = './dist'; // dossier à livrer
+
+gulp.task('sass', function () {
+    return gulp.src('./sass/**/*.scss')
         .pipe(sass())
-        .pipe(gulp.dest('app/public/css'))
+        .pipe(gulp.dest('./dist/public/css'))
         .pipe(browserSync.reload({
             stream: true
         }))
 });
 
-gulp.task('useref', function(){
-    var assets = useref.assets();
+gulp.task('img', function () {
+    return gulp.src('./img/*.png')
+        .pipe(gulp.dest('./dist/public/img'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
+});
 
-    return gulp.src('app/*.html')
-        .pipe(assets)
-        .pipe(uglify())
-        .pipe(assets.restore())
-        .pipe(useref())
-        .pipe(gulp.dest('dist'))
+gulp.task('ejs', function () {
+    return gulp.src('./views/*.ejs')
+        .pipe(gulp.dest('./dist/views'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
+});
+
+gulp.task('partials', function () {
+    return gulp.src('./partials/*.ejs')
+        .pipe(gulp.dest('./dist/partials'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
+});
+
+gulp.task('app', function () {
+    return gulp.src('./app.js')
+        .pipe(gulp.dest('./dist'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
 });
 
 gulp.task('browserSync', ['nodemon'], function() {
     browserSync.init(null, {
-        proxy: "http://localhost:8080",
+        proxy: "http://localhost:8080", // port of node server
     });
 });
 
 gulp.task('nodemon', function (cb) {
     var callbackCalled = false;
-    return nodemon({script: './app/app.js'}).on('start', function () {
+    return nodemon({script: './dist/app.js'}).on('start', function () {
         if (!callbackCalled) {
             callbackCalled = true;
             cb();
@@ -41,8 +66,19 @@ gulp.task('nodemon', function (cb) {
     });
 });
 
-gulp.task('watch', ['browserSync', 'sass'], function(){
-    gulp.watch('app/scss/**/*.scss', ['sass']);
-    gulp.watch('app/app.js', browserSync.reload);
-    gulp.watch('app/views/*.ejs', browserSync.reload);
-})
+// gulp.task('watch', ['browserSync', 'ejs','partials', 'compass', 'img', 'server','nodemon'], function () {
+//     gulp.watch('./sass/*.scss', ['compass'])
+//     gulp.watch('./views/**/*.ejs', ['ejs'])
+//     gulp.watch('./partials/**/*.ejs', ['partials'])
+//     gulp.watch('./img/*.png', ['img'])
+//     gulp.watch('./server.js', ['server']);
+//     // Other watchers
+// });
+
+gulp.task('watch', ['browserSync', 'app', 'ejs','partials', 'sass', 'img'], function () {
+    gulp.watch(["./app.js"], reload)
+    gulp.watch(["./views/*.ejs"], reload)
+    gulp.watch(["./sass/*.scss"], reload)
+    gulp.watch(["./partials/*.ejs"], reload)
+    gulp.watch(["./img/*.png"], reload);
+});
