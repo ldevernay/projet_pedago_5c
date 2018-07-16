3bb2fa0b05c925441bbf190ceb7f3d2536b6d6bf
marion1998 <marion281098@outlook.fr>
2018-07-16 16:55:31 +0200

    win image

--- public/scripts/planter.js
+++ public/scripts/planter.js
@@ -1,6 +1,6 @@
-let winImages = ["graine_terre.jpg", "plantule.jpg", "plante.jpg", "patates.png", "peasant.jpg"];
+let winImages = ["plantule.jpg", "plante.jpg", "patates.png", "peasant.jpg"];
 let failImages = ["poule_mange.png", "limace_mange.png", "plante_sans_patates.png", "plante_fruits.png", "patate_germee.png"];
-let btnWinImages = ["arroser.png", "arroser.png", "amendement.png", "recolter.png", "camion.png", "camion.png"];
+let btnWinImages = [ "arroser.jpg", "soleil.png", "fumier.jpg", "recolter.jpeg", "recolter.png", "camion.png", "camion.png"];
 let btnFailImages = ["poule.png", "limace.png", "recolter.jpg", "arroser.png",  "planter.png", "planter.png"];
 
 //DOM selectors
