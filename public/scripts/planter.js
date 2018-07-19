bf8c4e069466f42d61e68af50b96f61f79c073c9
marion1998 <marion281098@outlook.fr>
2018-07-19 09:44:44 +0200

    patate cuisiné

--- public/scripts/planter.js
+++ public/scripts/planter.js
@@ -1,7 +1,7 @@
-let winImages = ["plantule.jpg", "plante.jpg", "patate.jpg", "patates.png", "potato_truck.jpeg", "potato.png"];
-let failImages = ["poule_mange.png", "escargot_mange.jpg", "feuille.png", "potato_sad.png", "potato_bike.png"];
-let btnWinImages = [ "arroser.jpg", "soleil.png", "recolter.jpeg", "camion.png", "market.png", ""];
-let btnFailImages = ["escargot.png", "vent.png", "arracher.png", "velo.png", "house.jpg", ""];
+let winImages = ["plantule.jpg", "plante.jpg", "patate.jpg", "patates.png", "potato_truck.jpeg", "market_buy.jpg", "potato_house.jpg", "cooked_potato.jpg"];
+let failImages = ["poule_mange.png", "escargot_mange.jpg", "feuille.png", "potato_sad.png", "potato_bike.png", "house_potato.png","dog_eat.png", "puree.jpg"];
+let btnWinImages = [ "arroser.jpg", "soleil.png", "recolter.jpeg", "camion.png", "market.png", "parent.jpg", "pan.png", ""];
+let btnFailImages = ["escargot.png", "vent.png", "arracher.png", "velo.png", "house.jpg", "dog.png", "pressoir.png", ""];
 
 //DOM selectors
 // let imgCirlce = document.getElementsByClassName('img.circle');
