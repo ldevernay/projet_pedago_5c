1c295a270f1e249dadac58cd75d54c039c40f543
marion1998 <marion281098@outlook.fr>
2018-07-19 14:57:38 +0200

    json

--- public/scripts/planter.js
+++ public/scripts/planter.js
@@ -1,8 +1,9 @@
-let winImages = ["plantule.jpg", "plante.jpg", "patate.jpg", "patates.png", "potato_truck.jpeg", "market_buy.jpg", "potato_house.jpg", "cooked_potato.jpg"];
-let failImages = ["poule_mange.png", "escargot_mange.jpg", "feuille.png", "potato_sad.png", "potato_bike.png", "house_potato.png","dog_eat.png", "puree.jpg"];
-let btnWinImages = [ "arroser.jpg", "soleil.png", "recolter.jpeg", "camion.png", "market.png", "parent.jpg", "pan.png", ""];
-let btnFailImages = ["escargot.png", "vent.png", "arracher.png", "velo.png", "house.jpg", "dog.png", "pressoir.png", ""];
-
+// let winImages = ["plantule.jpg", "plante.jpg", "patate.jpg", "patates.png", "potato_truck.jpeg", "market_buy.jpg", "potato_house.jpg", "cooked_potato.jpg"];
+// let failImages = ["poule_mange.png", "escargot_mange.jpg", "feuille.png", "potato_sad.png", "potato_bike.png", "house_potato.png","dog_eat.png", "puree.jpg"];
+// let btnWinImages = [ "arroser.jpg", "soleil.png", "recolter.jpeg", "camion.png", "market.png", "parent.jpg", "pan.png", ""];
+// let btnFailImages = ["escargot.png", "nuage.png", "arracher.png", "velo.png", "house.jpg", "dog.png", "pressoir.png", ""];
+// let text1 = ["Arroser", "Soleil", "Récolter", "ECamion", "Marché", "Acheter", "Poêle"]
+// let text2 = ["Un escargot ?", "Pluie", "Arracher", "Vélo ?", "Maison ", "Animal", "Ecrasé"]
 //DOM selectors
 // let imgCirlce = document.getElementsByClassName('img.circle');
 
@@ -16,6 +17,8 @@ let changeBtn1Image = () => {
     if(!gameOver && btnCounter < btnWinImages.length){
         document.getElementById("button1").src = `./images/${btnWinImages[btnCounter]}`;
         document.getElementById("button2").src = `./images/${btnFailImages[btnCounter]}`;
+        document.getElementById('text1').innerHTML = text1[btnCounter];
+        document.getElementById('text2').innerHTML = text2[btnCounter];
         changeMainImage(winImages[btnCounter]);
         btnCounter++;
     }else{
