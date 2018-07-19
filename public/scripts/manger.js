ceb9c0b9454c89f83a3f940c099790a10e05d18f
Gyozmo <binantguillaume@gmail.com>
2018-07-19 10:06:00 +0200

    Module 2 fonctions

--- public/scripts/manger.js
+++ public/scripts/manger.js
@@ -23,6 +23,7 @@ $(document).ready(function () {
                 // .find('.food')
                 ui.draggable.css("color", "red");
 
+                
 
 
                 if (ui.draggable.is(":not(.foodAssiette)")) {
@@ -34,11 +35,40 @@ $(document).ready(function () {
                 ui.draggable.addClass('foodAssiette')
 
                 console.log('result = ' + result)
-                // console.log($(this).find('.food'));
+                // $('.counter').html(result)
+
+                // if(result < 0 && result > -20){
+                //     $('.counter').html("sad")
+                // } else if (result < -20) {
+                //     $('.counter').html("very sad")
+                // } else if (result >= 0 && result < 20) {
+                //     $('.counter').html("neutral")
+                // } else if (result > 20 && result < 40) {
+                //     $('.counter').html("sad")
+                // } else if (result>40 && result < 60){
+                //     $('.counter').html("happy")
+                // } else {
+                //     $('.counter').html("ate too much")
+                // }
+                console.log($(this).find('.food'));
+
+                 if(result > 59){
+                    $('.counter').html("too much")
+                } else if (result > 39) {
+                    $('.counter').html("very good")
+                } else if ( result > 19) {
+                    $('.counter').html("good")
+                } else if (result < -39) {
+                    $('.counter').html("very sad")
+                } else if (result < -19) {
+                    $('.counter').html("sad")
+                } else {
+                    $('.counter').html("neutral")
+                }
             }
 
 
-        })
+     })
 
         $("#table").droppable({
             drop: function (event, ui) {
@@ -48,26 +78,58 @@ $(document).ready(function () {
                 ui.draggable.css("color", "blue")
 
 
-                if (ui.draggable.is(":not(.foodTable)")) {
-                    console.log('dont have class Table');
+
+                if (ui.draggable.is(":not(.foodAssiette)") && ui.draggable.is(":not(.foodTable)")) {
+                    console.log('no divs');
+                    
+                } else if (ui.draggable.is(":not(.foodTable)")) {
+                    console.log('dont have class assiette');
                     result -= Number(ui.draggable.attr('data-score'));
                 }
+
+
 
                 ui.draggable.removeClass('foodAssiette')
                 ui.draggable.addClass('foodTable')
 
                 console.log('result = ' + result);
 
-
+                // $('.counter').html(result)
+                // if(result < 0 && result > -20){
+                //     $('.counter').html("sad")
+                // } else if (result < -20) {
+                //     $('.counter').html("very sad")
+                // } else if (result >= 0 && result < 20) {
+                //     $('.counter').html("neutral")
+                // } else if (result > 20 && result < 40) {
+                //     $('.counter').html("sad")
+                // } else if (result>40 && result < 60){
+                //     $('.counter').html("happy")
+                // } else {
+                //     $('.counter').html("ate too much")
+                // }
                 // console.log($(this).find('.food'));
+                if(result > 59){
+                    $('.counter').html("too much")
+                } else if (result > 39) {
+                    $('.counter').html("very good")
+                } else if ( result > 19) {
+                    $('.counter').html("good")
+                } else if (result < -39) {
+                    $('.counter').html("very sad")
+                } else if (result < -19) {
+                    $('.counter').html("sad")
+                } else {
+                    $('.counter').html("neutral")
+                }
+                
+                
             }
-
-
         })
 
+        
+        
 
 
-    });
-
-
-});
+    });//function end
+});//ready end
